277dca2943b881d28b12ad8f1fe7dfee6ac70eed
熊伟 <43609346+xiongweii@users.noreply.github.com>
2019-04-15 14:48:02 +0800

    Update plotData.py

--- machine-learning-ex1/ex1/plotData.py
+++ machine-learning-ex1/ex1/plotData.py
@@ -13,5 +13,8 @@ def plot_data(x, y):
 
 
     # ===========================================================
-
+    plt.scatter(x,y,'b','x')
+    plt.xlabel("Population of City in 10,000s")
+    plt.ylabel("Profit in $10,000s")
+    
     plt.show()
